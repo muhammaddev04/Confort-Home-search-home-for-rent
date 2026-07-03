4b70feb542d25f31dbdb09237efa9914c59cb173
muhammaddev04 <muhammadmahmudzoda04@gmail.com>
2026-07-03 07:52:49 +0500

    find_relevant_properties function

--- property/views.py
+++ property/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .models import Property, Property_Image, Favorite
+from .forms import PropertyForm, PropertyImageForm
+from django.http import JsonResponse
+from django.db.models import Avg
+import os
+from django.http import JsonResponse
+from groq import Groq
+from dotenv import load_dotenv
+from django.views.decorators.csrf import csrf_exempt
+import json
+from .models import Property, Favorite, Message
 
-# Create your views here.
+
+
+
+load_dotenv()
+
+api_key = os.getenv("GROQ_API_KEY")
+
+
+AI_RATE_LIMIT = 15
+AI_RATE_WINDOW = 60  
+
+AI_REPLY_LANGUAGES = {
+    'tg': 'Tajik',
+    'ru': 'Russian',
+    'en': 'English',
+}
+
+
+_CITY_KEYWORDS = ['dushanbe', 'душанбе', 'khujand', 'хуҷанд', 'худжанд', 'kulob', 'кӯлоб', 'куляб', 'bokhtar', 'бохтар']
+_TYPE_KEYWORDS = {
+    'apartment': ['apartment', 'квартира', 'квартираи', 'flat'],
+    'house': ['house', 'ҳавлӣ', 'хонаи', 'дом', 'ҳавлигӣ'],
+    'room': ['room', 'ҳуҷра', 'комната'],
+}
+
+
+def _find_relevant_properties(user_message):
+    
+    text = user_message.lower()
+    qs = Property.objects.filter(is_available=True)
+
+    for city in _CITY_KEYWORDS:
+        if city in text:
+            qs = qs.filter(city__icontains=city.split()[0][:4])
+            break
+
+    for ptype, keywords in _TYPE_KEYWORDS.items():
+        if any(kw in text for kw in keywords):
+            qs = qs.filter(property_type=ptype)
+            break
+
+    return qs.order_by('-created_at')[:5]
+
+
