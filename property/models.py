6bdc93053a3d5d76b3b600f8f202508ec5005142
muhammaddev04 <muhammadmahmudzoda04@gmail.com>
2026-07-03 07:12:04 +0500

    Create property models

--- property/models.py
+++ property/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Property(models.Model):
+    PROPERTY_CHOICES = [
+        ('apartment', 'Apartment'),
+        ('house', 'House'),
+        ('room', 'Room'),
+    ]
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    property_type = models.CharField(max_length=50, choices=PROPERTY_CHOICES)
+    price = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    rooms = models.PositiveIntegerField()
+    area = models.DecimalField(max_digits=10, decimal_places=2)
+    floor = models.PositiveIntegerField(null=True,blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
+    district = models.CharField(max_length=100)
+    address_details = models.TextField()
+    amenities = models.TextField()
+    is_available = models.BooleanField(default=True)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title} — {self.price} TJS'
+
